d4f0aca23d8f3a66553a487e7152a628c57426b1
Gandalf <callanlatimer@outlook.com>
2022-08-01 22:38:58 -0400

    Final Commit- have to add live link to README after it is created

--- CodeQuiz.js
+++ CodeQuiz.js
@@ -1,14 +1,15 @@
 var startButton = document.querySelector("#start");
-//var nextButton = document.querySelector("#next");
-var highscoreButton= document.querySelector("#highscore-form-btn");
+var resetButton = document.querySelector("#reset");
+var submitButton= document.querySelector("#name-button");
+var highscoreForm = document.querySelector("#highscore-form");
 var timerEl = document.querySelector("#timer");
-var highscore = document.querySelector("highscore-form"); 
 var questionEl = document.querySelector("#quiz");
 var answerEl = document.querySelector("#answers");
 var instructions = document.querySelector("#instructions")
 var startPageEl= document.querySelector("#startpage");
-var submitButton = document.getElementById("submit"); 
-var highscore = document.querySelector("#highscore")
+var highscore = document.querySelector("#highscore");
+var championHall = document.querySelector("#hall");
+var input = document.querySelector("#champion")
 var questionNumber= 0;
 var answerNumber=0;
 var numCorrect = 0;
@@ -22,48 +23,57 @@ var questionArray = {
         "What is Javascript?", 
         "what is the purpose of .concat()?",
         "In CSS, how can you individualize property assignments to HTML elements?",
-        "In CSS, how can you prevent redundancies?"
+        "In CSS, how can you prevent redundancies?",
+        "Which of these is not a semantic element in HTML?"
     ],
 
 answers: [
         [
-        "Correct: A coding language that makes a page interactive", 
-        "Fancy latte art",
-         "Pure despair",
-         "A third party API"
+          "Correct: A coding language that makes a page interactive", 
+          "Fancy latte art",
+          "Pure despair",
+          "A third party API"
         ],
 
         [
-        "Conning small mammals",
-        "Correct: Adding the content of arrays together",
-        "Obtaining a random number",
-        "Taking arrays apart"
+          "Conning small mammals",
+          "Correct: Adding the content of arrays together",
+          "Obtaining a random number",
+          "Taking arrays apart"
         ],
 
         [
-        "By giving them a special name",
-        "By sorting them into their own Hogwarts House",
-        "Correct: By using the class or ID selector",
-        "MDN because it never lies"
+          "By giving them a special name",
+          "By sorting them into their own Hogwarts House",
+          "Correct: By using the class or ID selector",
+          "Watching cartoons"
         ],
 
-        ["Just don't do it",
-        "A reset CSS file",
-        "Correct: Using root:",
-        "MDN because it never lies"
+        [
+          "Just don't do it",
+          "A reset CSS file",
+          "Correct: Using root:",
+          "MDN because it never lies"
+        ],
+
+        [
+          "main",
+          "Correct: div",
+          "header",
+          "footer"
         ]
+
 
     ]
 
  };
 
- console.log(questionArray);
-
     
 startButton.addEventListener("click", beginQuiz);
 answerEl.addEventListener("click", checkCorrect);
 
-    
+
+ // timer element with parameters to clear the interval if the secondsLeft reaches 0 and call the endQuiz function   
 function startTimer() {
     timerEl.style.display = "block"; 
    
@@ -82,11 +92,7 @@ function startTimer() {
       return;
 };
 
-
-console.log(startTimer);
-
-
-
+//sets the gameover value to false, starts the timer, shows question choices, and hides the startbutton/instructions
 function beginQuiz(){
     gameOver = false;
     secondsLeft = 60;
@@ -95,62 +101,74 @@ function beginQuiz(){
     showQuestions();
      startButton.style.display = "none";
      document.querySelector("#instructions").style.display = "none";
+     document.querySelector("#scoreboard").style.display= "none";
+     document.querySelector("footer").style.display = "none"
 };
 
-console.log(beginQuiz);
+// console.log(beginQuiz);
 
+//sets the inner text of the question element to the question number within the question array (some unknown bug not displaying next question)
 function showQuestions() {
-    questionEl.textContent = questionArray.questions[questionNumber];
     showChoices();
  };
 
- console.log(showQuestions);
+//  console.log(showQuestions);
 
+ //sets the HTML text to null; 
  function showChoices() {
 
     answerEl.innerHTML = '';
-    for (answerNumber= 0; answerNumber < (questionArray.answers[questionNumber].length); answerNumber++){
+     questionEl.textContent = questionArray.questions[questionNumber];
+    //Answer number inital value is set to 0, runs the loop for the length value of the question array
+    for (let answerNumber= 0; answerNumber < (questionArray.answers[questionNumber].length); answerNumber++){
+        //current choice variable creates a list element
         var currentChoice = document.createElement("li");
+        //fade variable is equal to the value the answers value within the quesiton array
         var fade = questionArray.answers[questionNumber][answerNumber];
+
+        //if a given value within the answers array has the id "Correct" attached to it, remove the text value of that id so 
+        // it doesnt show up on the actual quiz app
 
         if(questionArray.answers[questionNumber][answerNumber].includes("Correct:")){
             fade = questionArray.answers[questionNumber][answerNumber].substring(8, questionArray.answers[questionNumber][answerNumber].length);
             currentChoice.id= "Correct";
            
         }
+            //sets the text content of the list element 
             currentChoice.textContent= fade;
+            //appends list elements to the parent so that all of the options are displayed
             answerEl.appendChild(currentChoice);
+            
     }
     return;
 
  };
 
- console.log(showChoices);
-
+//ends quiz if questionNumber is greater than the length of the question array in var questionArray; otherwise the quiz keeps cycling
  function nextChoice() {
     questionNumber++;
+
     if (questionNumber >= questionArray.questions.length){
         endQuiz();
     } else {
         showChoices();
     }
-    return;
+   
  };
 
- console.log(nextChoice);
-
+ //ends quiz and hides/shows appropriate elements
  function endQuiz(){
     gameOver= true;
     timerEl.style.display = "none";
-    questionEl.style.display = "none"
+    questionEl.style.display = "none";
     answerEl.innerHTML = '';
     highscore.textContent= numCorrect;
     document.querySelector("#highscore-form").style.display= "block";
+    document.querySelector("#scoreboard").style.display= "block";
     return;
  }
 
-console.log(endQuiz);
-
+// looks for "Correct:" id and adds a value of one to the the score if the answer is correct otherwise it deducts ten seconds from secondsLeft
 function checkCorrect(event) {
         if (event.target=answerEl){
             console.log(event.target)
@@ -168,4 +186,32 @@ function checkCorrect(event) {
 } 
     return;
 };
+
+//saves highscores to local storage and displays them in the score section
+submitButton.addEventListener("click", function(event){
+event.preventDefault();
+var highscoreInput = input.value;
+var highscores = {
+    name: highscoreInput,
+    scores: numCorrect
+};
+
+localStorage.setItem("highscores", JSON.stringify(highscores))
+
+});
+
+
+function renderScore(){
+    var lastScore = JSON.parse(localStorage.getItem("highscores"));
+    console.log(lastScore.name)
+    if (lastScore !== null){
+        document.getElementById("list").textContent = lastScore.name + " " + lastScore.scores
+    }
+
+};
+
+renderScore();
+
+ 
+
 
--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # Challenge4_CodeQuiz
+
+# Description of Project
+The purpose of this project was to generate an interactive code quiz which uses dynamic HTML, CSS, and Javascript. Through the traversal of the DOM, this application was created. 
+
+## Functionality
+[HTML]
+* Header element contains the timer inside a `<p>` element; seconds is the text content and the value of the time remaining is within the Javascript
+* Created a hero element for style
+* `<h2>` element contains the id quiz which is the value used to insert the quiz question
+* Begin button which starts the game when clicked
+* Empty `<ul>` element to contain answer choices dynamically created in java
+* Highscore form `<div>` used to contain the end page with the score submit button, reset button, and text input
+* Scoreboard element to keep track of user scores and initials; appends new list item with each new score
+
+[CSS]
+* Set variables at the beginning to prevent redundancy
+* Use display: none in CSS which can be dynamically changed in Java to hide/show elements
+* main > ul li used so that styling will apply as elements are created (since they do not exist before Java is applied)
+* Hover element applied to answer choices so that user knows which answer they are selecting
+
+[Javascript]
+* set all global variables and querySelectors at the beginning of the file
+* var for questions and answers is in an array
+* EventListeners listen for a click and run the corresponding function
+* `startTimer()` displays the timer and sets the interval; begins countdown and ends the quiz if the timer reaches 0
+* `beginQuiz` displays the questions/answers and hides all other unrelated elements
+* `showChoices` takes the answerNumber and gives it an index value of 0; 
+the for loop runs for as long as the answerNumber value is less than the value of the length of the answers array within the questionArray variable; 
+* `var currentChoice` creates a list item for each answer to display;
+* "if" loop selects any choice with the id "Correct" in front of it and uses the substring method to remove the exact amount of string characters that id amounts to so that it doesn't show up on the actual answer buttons;
+* Set the textContent of the list elements to the new text output created in the if loop and append the children to the `<ul>` parent element.
+* `nextChoice()` adds a value of one to the questionNumber variable as the  quiz progresses from question to question; if statement stops the quiz if the questionNumber is higher than the number of questions within `var questionArray`
+* `endQuiz` sets the value of gameover to true and hides the timer, question, and answer elements; It loads the scorepage and displays the score value which is equal to the number of correct questions
+* `checkCorrect` targets the "correct:" id within the question array.
+  If it is correct, the number of correct questions(score) increases and the window displays an alert that says "correct"
+  If the "correct:" id is not within the answer selected, the window displays a "wrong" alert and ten seconds is deducted from secondsLeft
+* The `EventListener` for the submit button triggers a prevent default method. `var highscoreInput` is equal to whatever the user puts into the input box on the score page. The variables `name` and `scores` are saved to local storage
+* `renderScore` takes these values from local storage and converts them to a text value which can be displayed on the page under the scoreboard.
+If statement says that if lastScore is not equal to nothing, display the text content to the page. 
+* When the user refreshes the page, they can see their previous score and attempt the quiz again. 
+
+
+
+
+
+
+## Assets
+[!Screenshot](./Images/CodeQuizScrnsht.jpeg)
+[LinktoRepository](https://github.com/Clatimer97/Challenge4_CodeQuiz)
+[LinktoLiveApp]()
+
+# References
+* For my reset stylesheet, I used the Eric Meyer reset stylesheet which is a standardized reset stylsheet available on the internet. The link to reference it is posted below.
+[LinktoStylesheet](https://meyerweb.com/eric/tools/css/reset/)
+
+## Author
+[@CallanLatimer](https://github.com/Clatimer97)
